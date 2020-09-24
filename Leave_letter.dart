6e86b1f86a7e45d2af5ca1b83ad13904fea195e3
Gomathi4468 <71780516+Gomathi4468@users.noreply.github.com>
2020-09-24 13:05:38 +0530

    Create Leave_letter.dart

---
+++ Leave_letter.dart
@@ -0,0 +1,12 @@
+void main(){
+  print("\t\t\tLEAVE LETTER");
+  print("From:");
+  print("\tP.Gomathi,\n\tNo:26 3rd Cross,\n\tThirubuvanai,\n\tPuducherry.");
+  print("To:");
+  print("\tThe Class Teacher,\n\tSVHS School,\n\tPuducherry.");
+  print("Respected Sir,");
+  print("\tAs I am suffering from fever. I am not unable to attend the class. So I request you to give me a permission to take a leave for two days  from 24.09.2020 to 25.09.2020.");
+  print("\t\t\tThanking You,");
+  print("Date:23.09.2020\t\t\t\t\tYours Faithfully,");
+  print("Place:Puducherry\t\t\t\t Gomathi.P");
+}
